850aba3850fd04aea6348b99d2f4b794e5290b95
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2020-09-13 23:55:48 +0900

    Upload initial code

---
+++ 30 Days of Code/5 Loop.cpp
@@ -0,0 +1,15 @@
+// problem source: https://www.hackerrank.com/challenges/30-loops/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    return 0;
+}
