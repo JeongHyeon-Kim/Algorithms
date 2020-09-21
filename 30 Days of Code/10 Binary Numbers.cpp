7069f8e4ea2160fab80c61f53f5e1d073c657851
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-09-21 23:44:18 +0900

    Upload basic code of 'Binary Numbers'

---
+++ 30 Days of Code/10 Binary Numbers.cpp
@@ -0,0 +1,16 @@
+// problem source: https://www.hackerrank.com/challenges/30-binary-numbers/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    return 0;
+}
