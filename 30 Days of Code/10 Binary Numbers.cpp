3f4fb6bae542d92e002c3a9c9970592c2c4f71ca
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-09-22 00:05:51 +0900

    Solved 'Binary Numbers'

--- 30 Days of Code/10 Binary Numbers.cpp
+++ 30 Days of Code/10 Binary Numbers.cpp
@@ -1,16 +1,29 @@
 // problem source: https://www.hackerrank.com/challenges/30-binary-numbers/problem
-
 #include <bits/stdc++.h>
 
 using namespace std;
-
-
 
 int main()
 {
     int n;
     cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int count = 0, max = 0;
+
+    while (n > 0) {
+        if (n % 2 == 1) {
+            count += 1;
+            if (max < count) {
+                max = count;
+            }
+        } else {
+            count = 0;
+        }
+        n = n / 2;
+    }
+
+    cout << max;
 
     return 0;
 }
