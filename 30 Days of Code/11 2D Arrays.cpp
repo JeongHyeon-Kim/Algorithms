e79004a423b76535a73627f7062b5fde04de6f97
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2020-09-27 23:46:03 +0900

    Upload basic code of '2D Arrays'

---
+++ 30 Days of Code/11 2D Arrays.cpp
@@ -0,0 +1,20 @@
+// problem source: https://www.hackerrank.com/challenges/30-2d-arrays/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    return 0;
+}
