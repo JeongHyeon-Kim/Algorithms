6ba2f7c864e11d6da64725d88ac5a4f0c3fd3bdc
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-10-05 23:56:35 +0900

    Solved '2D Arrays'

--- 30 Days of Code/11 2D Arrays.cpp
+++ 30 Days of Code/11 2D Arrays.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main()
 {
     vector<vector<int>> arr(6);
+    int max = INT_MIN; // minimum value of int
+    int sum = 0;
     for (int i = 0; i < 6; i++) {
         arr[i].resize(6);
 
@@ -16,5 +18,16 @@ int main()
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
+    for (int search_i = 0; search_i < 4; search_i++) {
+        for (int search_j = 0; search_j < 4; search_j++) {
+            sum = 0;
+            sum = arr[search_i][search_j] + arr[search_i][search_j + 1] + arr[search_i][search_j + 2];
+            sum += arr[search_i + 1][search_j + 1];
+            sum += arr[search_i + 2][search_j] + arr[search_i + 2][search_j + 1] + arr[search_i + 2][search_j + 2];
+            if (max < sum)
+                max = sum;
+        }
+    }
+    cout << max << endl;
     return 0;
 }
