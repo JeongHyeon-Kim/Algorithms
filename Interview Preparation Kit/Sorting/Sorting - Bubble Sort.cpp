1fdcc510fd5df611845d9be944eeab24c983397b
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-10-20 19:40:48 +0900

    Solved "Sorting - Bubble Sort"

---
+++ Interview Preparation Kit/Sorting/Sorting - Bubble Sort.cpp
@@ -0,0 +1,77 @@
+// problem source: https://www.hackerrank.com/challenges/ctci-bubble-sort/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the countSwaps function below.
+void countSwaps(vector<int> a) {
+    int n = a.size(), count = 0;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > a[j]) {
+                int tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                count++;
+            }
+        }
+    }
+    cout << "Array is sorted in " << count << " swaps."<< endl;
+    cout << "First Element: " << a[0] << endl;
+    cout << "Last Element: " << a[n-1] << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    countSwaps(a);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
