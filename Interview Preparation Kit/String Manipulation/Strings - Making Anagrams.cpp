c12cb7bd8ef7ca9e5868c56cb9d396fdbb5127cf
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-10-24 23:37:12 +0900

    Upload basic code of 'Making Anagrams'

---
+++ Interview Preparation Kit/String Manipulation/Strings - Making Anagrams.cpp
@@ -0,0 +1,58 @@
+// https://www.hackerrank.com/challenges/ctci-making-anagrams/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the makeAnagram function below.
+int makeAnagram(string a, string b) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string a;
+    getline(cin, a);
+
+    string b;
+    getline(cin, b);
+
+    int res = makeAnagram(a, b);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the makeAnagram function below.
+int makeAnagram(string a, string b) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string a;
+    getline(cin, a);
+
+    string b;
+    getline(cin, b);
+
+    int res = makeAnagram(a, b);
+
+    fout << res << "\n";
+
+    fout.close();
+
+    return 0;
+}
