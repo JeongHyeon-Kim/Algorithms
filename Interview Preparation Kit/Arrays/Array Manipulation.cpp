51fc13a6014953b0643b42f12ca8898ba9486f68
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-11-08 17:47:35 +0900

    Solved 'Array Manipulation'

--- Interview Preparation Kit/Arrays/Array Manipulation.cpp
+++ Interview Preparation Kit/Arrays/Array Manipulation.cpp
@@ -1,4 +1,5 @@
 // problem source: https://www.hackerrank.com/challenges/crush/problem
+// discussion reference: https://www.hackerrank.com/challenges/crush/forum/comments/69550
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -8,34 +9,52 @@ vector<string> split_string(string);
 // Complete the arrayManipulation function below.
 long arrayManipulation(int n, vector<vector<int>> queries) {
     // Did not execute within the time limits
-    long max = 0;
-    int test_case = queries.size();
-    vector<long> updating_arr(n);
-    int start, end, min_start = 0, max_end = 0;
-    for (int i = 0; i < test_case; i++) {
-        start = queries[i][0] - 1;
-        end = queries[i][1];
-        if (min_start > start)
-            min_start = start;
-        if (max_end < end)
-            max_end = end;
-        for (int j = start; j < end; j++)
-            updating_arr[j] += queries[i][2];
-
-    }
-
+    // long max = 0;
+    // int test_case = queries.size();
+    // vector<long> updating_arr(n);
+    // int start, end, min_start = 0, max_end = 0;
+    // for (int i = 0; i < test_case; i++) {
+    //     start = queries[i][0] - 1;
+    //     end = queries[i][1];
+    //     if (min_start > start)
+    //         min_start = start;
+    //     if (max_end < end)
+    //         max_end = end;
+    //     for (int j = start; j < end; j++)
+    //         updating_arr[j] += queries[i][2];
+    //
+    // }
     // summing method 1
-    for (int i = min_start; i < max_end; i++) {
-        if (max < updating_arr[i])
-            max = updating_arr[i];
-    }
-    return max;
-
+    // for (int i = min_start; i < max_end; i++) {
+    //     if (max < updating_arr[i])
+    //         max = updating_arr[i];
+    // }
+    // return max;
     // summing method 2
     // vector<long>::iterator max_pos;
     // max_pos = max_element(updating_arr.begin(), updating_arr.end());
     // int get_diff = std::distance(updating_arr.begin(), max_pos);
     // return updating_arr[get_diff];
+    long max = 0, comp = 0;
+    int test_case = queries.size();
+    vector<long> updating_arr(n + 1); // not using index 0 of updating_arr
+
+    for (int i = 0; i < test_case; i++) {
+        int start = queries[i][0];
+        int end = queries[i][1];
+        int value = queries[i][2];
+
+        updating_arr[start] += value; // start point of including
+        if ((end + 1) <= n)
+            updating_arr[end + 1] -= value; // end point of not including
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << updating_arr[i] << " ";
+        comp += updating_arr[i];
+        if (max < comp)
+            max = comp;
+    }
+    return max;
 }
 
 int main()
