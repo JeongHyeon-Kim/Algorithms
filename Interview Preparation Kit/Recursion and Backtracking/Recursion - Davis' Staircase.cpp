ca949d4ac3f173f51784d5ee9b910942dd68a52f
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-11-08 17:11:44 +0900

    Solved 'Recursion - Davis' Staircase'

--- Interview Preparation Kit/Recursion and Backtracking/Recursion - Davis' Staircase.cpp
+++ Interview Preparation Kit/Recursion and Backtracking/Recursion - Davis' Staircase.cpp
@@ -1,12 +1,36 @@
 // problem source: https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem
+// discussion reference: https://www.hackerrank.com/challenges/ctci-recursive-staircase/forum/comments/859768, https://www.hackerrank.com/challenges/ctci-recursive-staircase/forum/comments/227816
 #include <bits/stdc++.h>
 
 using namespace std;
 
 // Complete the stepPerms function below.
 int stepPerms(int n) {
-
-
+    // Code did not execute within the time limits
+    // if (n == 1)
+    //     return 1;
+    // else if (n == 2)
+    //     return 2;
+    // else if (n == 3)
+    //     return 4;
+    // else
+    //   return stepPerms(n - 1) + stepPerms(n - 2) + stepPerms(n - 3);
+    if (n == 1) {
+        return 1;
+    } else if (n == 2) {
+        return 2;
+    } else if (n == 3) {
+        return 4;
+    } else {
+        vector<int> summing(3);
+        summing[0] = 1;
+        summing[1] = 2;
+        summing[2] = 4;
+        for (int i = 3; i < n; i++) {
+            summing.push_back(summing[i - 1] + summing[i - 2] + summing[i - 3]);
+        }
+        return summing[n - 1];
+    }
 }
 
 int main()
