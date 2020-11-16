cf6f6081151232b88ee9079d13747bb1bea6bf3d
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2020-11-16 21:56:20 +0900

    Upload middle solution of 'Fraudulent Activity Notifications'

--- Interview Preparation Kit/Sorting/Fraudulent Activity Notifications.cpp
+++ Interview Preparation Kit/Sorting/Fraudulent Activity Notifications.cpp
@@ -7,8 +7,39 @@ vector<string> split_string(string);
 
 // Complete the activityNotifications function below.
 int activityNotifications(vector<int> expenditure, int d) {
-
-
+    int ex_size = expenditure.size();
+    // priority_queue<int, vector<int>, greater<int>> trailing_data;
+    vector<int> trailing_data;
+    int notification = 0;
+    cout << ex_size << endl;
+    for (int i = 0; i < ex_size; i++) {
+        // cout << expenditure[i] << " ";
+        if (i < d) {
+            trailing_data.push_back(expenditure[i]);
+        }
+        if (trailing_data.size() == d) {
+            sort(trailing_data.begin(), trailing_data.end(), less<int>());
+            for (auto it = trailing_data.begin(); it != trailing_data.end(); it++)
+                cout << *it << " ";
+            cout << endl;
+            // if (d % 2 == 1) {
+            //     cout << trailing_data[d % 2] << endl;
+            //     if (trailing_data[d % 2] * 2 <= expenditure[i])
+            //         notification++;
+            // } else {
+            //     double temp = trailing_data[d % 2 - 1] + trailing_data[d % 2];
+            //     cout << temp / 2 << endl;
+            //     if (temp <= expenditure[i]) {
+            //         notification++;
+            //     }
+            // }
+            // // cout << trailing_data.top() << " ";
+            trailing_data.erase(trailing_data.begin());
+            trailing_data.push_back(expenditure[i]);
+        }
+    }
+    // sort(trailing_data.begin())
+    return notification;
 }
 
 int main()
