2464a3884d90c4738c099c894d08405111bdd25f
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2020-12-21 23:07:30 +0900

    Upload middle solution of 'Balanced Brackets'

--- Interview Preparation Kit/Stack and Queues/Balanced Brackets.cpp
+++ Interview Preparation Kit/Stack and Queues/Balanced Brackets.cpp
@@ -5,8 +5,32 @@ using namespace std;
 
 // Complete the isBalanced function below.
 string isBalanced(string s) {
+    // Occured segmentation fault in 18 cases
+    int s_size = s.size();
+    stack<char> balanced;
+    char start_bracket[3] = {'(', '{', '['};
+    char end_bracket[3] =  {')', '}', ']'};
 
-
+    if (s_size % 2 == 1)
+        return "NO";
+    else {
+        for (int i = 0; i < s_size; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (s[i] == start_bracket[j])
+                    balanced.push(s[i]);
+                else if (s[i] == end_bracket[j]) {
+                    if (balanced.top() == start_bracket[j])
+                        balanced.pop();
+                    else
+                        return "NO";
+                }
+            }
+        }
+    }
+    if (balanced.empty())
+        return "YES";
+    else
+        return "NO";
 }
 
 int main()
