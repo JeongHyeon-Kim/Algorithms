4533285f911de4be512e27636a1f2e323537d541
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-02-07 14:16:07 +0900

    Solved 'Tree - Postorder Traversal'

---
+++ Problem Solving/Trees/Tree - Postorder Traversal.cpp
@@ -0,0 +1,65 @@
+// problem source: https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.
+Node is defined as
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+
+    void postOrder(Node *root) {
+        if (!root)
+            return;
+        else {
+            postOrder(root->left);
+            postOrder(root->right);
+            cout << root->data << " ";
+        }
+    }
+
+}; //End of Solution
