29d670fb8b170372b1b19af4277ba1235473a44a
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-02-09 00:28:42 +0900

    Solved 'Binary Search Tree - Insertion'

---
+++ Problem Solving/Trees/Binary Search Tree - Insertion.cpp
@@ -0,0 +1,86 @@
+// problem source: https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem
+// problem reference: https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+
+  	void preOrder(Node *root) {
+
+      	if( root == NULL )
+          	return;
+
+      	std::cout << root->data << " ";
+
+      	preOrder(root->left);
+      	preOrder(root->right);
+    }
+
+/*
+Node is defined as
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    Node * insert(Node * root, int data) {
+        if(root == NULL) {
+                return new Node(data);
+        } else {
+            Node* cur;
+            if(data <= root->data) {
+                cur = insert(root->left, data);
+                root->left = cur;
+            } else {
+                cur = insert(root->right, data);
+                root->right = cur;
+            }
+            return root;
+        }
+    }
+
+};
+
+int main() {
+
+    Solution myTree;
+    Node* root = NULL;
+
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+
+    myTree.preOrder(root);
+
+    return 0;
+}
