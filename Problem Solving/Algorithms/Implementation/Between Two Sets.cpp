07a5628232a6d2cac49966e384a0d3f88d4b8811
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-03-21 16:10:56 +0900

    Sovled 'Between Two Sets'

---
+++ Problem Solving/Algorithms/Implementation/Between Two Sets.cpp
@@ -0,0 +1,146 @@
+// problem source: https://www.hackerrank.com/challenges/between-two-sets/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+int gcd(int a, int b) {
+    while (b > 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int gcd(vector<int> input) {
+    int input_size = input.size();
+    int result = input[0];
+    for (int i = 1; i < input_size; i++)
+        result = gcd(result, input[i]);
+    return result;
+}
+
+int lcm(int a, int b) {
+    return a * (b / gcd(a, b));
+}
+
+int lcm(vector<int> input) {
+    int input_size = input.size();
+    int result = input[0];
+    for (int i = 1; i < input_size; i++)
+        result = lcm(result, input[i]);
+    return result;
+}
+
+int getTotalX(vector<int> a, vector<int> b) {
+    int a_size = a.size();
+    int b_size = b.size();
+    int lcm_result = lcm(a);
+    int gcd_result = gcd(b);
+    int count = 0;
+    for (int i = lcm_result, j = 2; i <= gcd_result; i = lcm_result * j, j++) {
+        if (gcd_result % i == 0)
+            count++;
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    string brr_temp_temp;
+    getline(cin, brr_temp_temp);
+
+    vector<string> brr_temp = split(rtrim(brr_temp_temp));
+
+    vector<int> brr(m);
+
+    for (int i = 0; i < m; i++) {
+        int brr_item = stoi(brr_temp[i]);
+
+        brr[i] = brr_item;
+    }
+
+    int total = getTotalX(arr, brr);
+
+    fout << total << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
