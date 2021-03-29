64a76a0a17cce0eb49e9283af756c3e4e8310870
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-03-30 00:05:27 +0900

    Solved 'Gemstones'

--- Problem Solving/Algorithms/Strings/Gemstones.cpp
+++ Problem Solving/Algorithms/Strings/Gemstones.cpp
@@ -5,7 +5,35 @@ using namespace std;
 
 // Complete the gemstones function below.
 int gemstones(vector<string> arr) {
-  
+    int arr_size = arr.size();
+    unordered_map<char, int> mapping[arr_size]; // char and count
+    int count = 0;
+    vector<vector<bool>> exist(arr_size, vector<bool>(26, false));
+    bool result[26];
+    fill_n(result, 26, true);
+    for (int i = 0; i < arr_size; i++) {
+        int string_size = arr[i].size();
+        string individual_string = arr[i];
+        for (int j = 0; j < string_size; j++)
+            mapping[i][individual_string[j]]++;
+    }
+    for (int i = 0; i < arr_size; i++) {
+        for (auto it = mapping[i].begin(); it != mapping[i].end(); it++) {
+            char individual_char = it->first;
+            exist[i][individual_char - 97] = true;
+        }
+    }
+    for (int i = 0; i < arr_size; i++) {
+        for (int j = 0; j < 26; j++) {
+            if (!exist[i][j])
+                result[j] = false;
+        }
+    }
+    for (int i = 0; i < 26; i++) {
+        if(result[i])
+            count++;
+    }
+    return count;
 }
 
 int main()
