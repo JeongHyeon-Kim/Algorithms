9cd739abdc72317672ad6e978c0c72cc6ed0759f
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-05-11 23:57:27 +0900

    Uploaded basic code of 'Beautiful Binary String'

---
+++ Problem Solving/Algorithms/Strings/Beautiful Binary String.cpp
@@ -0,0 +1,61 @@
+// problem source: https://www.hackerrank.com/challenges/beautiful-binary-string/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'beautifulBinaryString' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING b as parameter.
+ */
+
+int beautifulBinaryString(string b) {
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string b;
+    getline(cin, b);
+
+    int result = beautifulBinaryString(b);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
