312da6eb8be8d9326da649fcda92b4f557c2a96e
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-07-09 01:42:05 +0900

    Solved 'Insert Delete GetRandom O(1)'

--- Frequent Problems in Coding Tests/Insert Delete GetRandom O(1).cpp
+++ Frequent Problems in Coding Tests/Insert Delete GetRandom O(1).cpp
@@ -1,28 +1,38 @@
 // problem source: https://leetcode.com/problems/insert-delete-getrandom-o1/
+// discussion reference: https://o-tantk.github.io/posts/randomized-array/
 class RandomizedSet {
 public:
     /** Initialize your data structure here. */
     vector<int> data;
-    RandomizedSet() {
-        // vector or map?
-    }
+    RandomizedSet() {}
 
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     bool insert(int val) {
-        data.push_back(val);
-        return true;
+        auto it = find(data.begin(), data.end(), val);
+        if (it != data.end())
+            return false;
+        else {
+            data.push_back(val);
+            return true;
+        }
     }
 
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     bool remove(int val) {
-        auto it = std::remove(data.begin(), data.end(), val);
-        data.resize(it - data.begin());
-        return true;
+        auto it = find(data.begin(), data.end(), val);
+        if (it != data.end()) {
+            cout << *it << endl;
+            data.erase(it);
+            return true;
+        } else
+            return false;
     }
 
     /** Get a random element from the set. */
     int getRandom() {
-        return 0;
+        int range = data.size();
+        int randomIndex = rand() % range;
+        return data[randomIndex];
     }
 };
 
