b80561a39bb914e8d0d96db08a4cb78a06320579
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-07-13 17:56:52 +0900

    Uploaded basic code of 'Merge k Sorted Lists'

---
+++ Frequent Problems in Coding Tests/Merge k Sorted Lists.cpp
@@ -0,0 +1,18 @@
+// problem source: https://leetcode.com/problems/merge-k-sorted-lists/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // implement merge sort
+        lists_size = lists.size();
+    }
+};
