701bf5e672c4d80d3d02379762479b194fc91225
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-07-28 00:01:10 +0900

    Solved 'Median of Two Sorted Arrays'

--- Frequent Problems in Coding Tests/Median of Two Sorted Arrays.cpp
+++ Frequent Problems in Coding Tests/Median of Two Sorted Arrays.cpp
@@ -2,13 +2,17 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double result = 0;
+        vector<int> merged;
         int num1_size = nums1.size(), num2_size = nums2.size();
         for (int i = 0; i < num1_size; i++)
-            cout << nums1[i] << " ";
-        cout << endl;
+            merged.push_back(nums1[i]);
         for (int i = 0; i < num2_size; i++)
-            cout << nums2[i] << " ";
-        return result;
+            merged.push_back(nums2[i]);
+        int merged_size = merged.size();
+        sort(merged.begin(), merged.end());
+        if (merged_size % 2 == 1)
+            return merged[merged_size / 2];
+        else
+            return (merged[merged_size / 2 - 1] + merged[merged_size / 2]) / 2.0f;
     }
 };
