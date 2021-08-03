957538e69f65221e37da121ff80a6bee326f2a63
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-03 23:55:44 +0900

    Uploaded basic code of 'Minimum Remove to Make Valid Parentheses'

---
+++ Frequent Problems in Coding Tests/Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,12 @@
+// problem source: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        string result;
+        int s_length = s.length();
+        for (int i = 0; i < s_length; i++) {
+            cout << s[i] << " ";
+        }
+        return result;
+    }
+};
