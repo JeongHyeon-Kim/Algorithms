37a8566f3f4e6406b739433db62b83b217a6a879
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-04 20:36:19 +0900

    Solved 'K Closest Points to Origin'

--- Frequent Problems in Coding Tests/K Closest Points to Origin.cpp
+++ Frequent Problems in Coding Tests/K Closest Points to Origin.cpp
@@ -3,15 +3,46 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         int x_max = points.size();
-        int y_max = points[0].size();
+        multimap<int, vector<int>> middle; // distance, location (accept redundancy)
         vector<vector<int>> result;
         for (int x = 0; x < x_max; x++) {
-            for (int y = 0; y < y_max; y++) {
-                cout << points[x][y] << " ";
+            int square = 0;
+            vector<int> location;
+            for (int y = 0; y < 2; y++) {
+                location.push_back(points[x][y]);
+                square += points[x][y] * points[x][y];
             }
-            cout << endl;
+            int fake_distance = square;
+            middle.insert(pair<int, vector<int>>(fake_distance, location));
         }
-        cout << k << endl;
+        auto it = middle.begin();
+        for (int i = 0; i < k && it != middle.end(); i++, it++) {
+            vector<int> temp = it->second;
+            result.push_back(temp);
+        }
         return result;
+
+        // Runtime Error with [[0,1],[1,0]], 2
+        // int x_max = points.size();
+        // unordered_map<int, vector<int>> middle; // distance, location
+        // vector<vector<int>> result;
+        // for (int x = 0; x < x_max; x++) {
+        //     int square = 0;
+        //     vector<int> location;
+        //     for (int y = 0; y < 2; y++) {
+        //         location.push_back(points[x][y]);
+        //         square += points[x][y] * points[x][y];
+        //     }
+        //     int fake_distance = square;
+        //     middle[fake_distance] = location;
+        // }
+        // std::map<int, vector<int>> ordered(middle.begin(), middle.end());
+        // auto it = ordered.begin();
+        // for (int i = 0; i < k; i++) {
+        //     vector<int> temp = it->second;
+        //     result.push_back(temp);
+        //     it++;
+        // }
+        // return result;
     }
 };
