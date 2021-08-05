684a764680b59ecf8f3135743a6c96a4670647d1
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-05 23:50:27 +0900

    Solved 'Minimum Remove to Make Valid Parentheses'

--- Frequent Problems in Coding Tests/Minimum Remove to Make Valid Parentheses.cpp
+++ Frequent Problems in Coding Tests/Minimum Remove to Make Valid Parentheses.cpp
@@ -1,12 +1,25 @@
-// problem source: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+// problem source: https: //leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+// discussion reference: https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/discuss/1381180/Stack-oror-faster-than-98.02-of-C%2B%2B-online-submissions
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        string result;
+        stack<int> s_index;
         int s_length = s.length();
         for (int i = 0; i < s_length; i++) {
-            cout << s[i] << " ";
+            if (s[i] == '(')
+                s_index.push(i);
+            else if (s[i] == ')') {
+                if (!s_index.empty())
+                    s_index.pop();
+                else
+                    s[i] = '*';
+            }
         }
-        return result;
+        while (!s_index.empty()) {
+            s[s_index.top()] = '*';
+            s_index.pop();
+        }
+        s.erase(remove(s.begin(), s.end(), '*'), s.end()); // erase(start, end), remove(begin, end, comp)
+        return s;
     }
 };
