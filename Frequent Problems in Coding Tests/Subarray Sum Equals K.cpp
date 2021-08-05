233d1c07ee264e49a4ec037195a9df1acd363232
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-06 00:30:38 +0900

    Uploaded basic code of 'Subarray Sum Equals K'

---
+++ Frequent Problems in Coding Tests/Subarray Sum Equals K.cpp
@@ -0,0 +1,13 @@
+// problem source: https://leetcode.com/problems/subarray-sum-equals-k/
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int nums_size = nums.size();
+        for (int i = 0; i < nums_size; i++) {
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+        cout << k << endl;
+        return 0;
+    }
+};
