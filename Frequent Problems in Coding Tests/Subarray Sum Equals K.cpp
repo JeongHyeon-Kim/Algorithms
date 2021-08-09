bde44fe4ea2ce32e8f348814d34316136beb925f
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-09 22:02:11 +0900

    Solved 'Subarray Sum Equals K'

--- Frequent Problems in Coding Tests/Subarray Sum Equals K.cpp
+++ Frequent Problems in Coding Tests/Subarray Sum Equals K.cpp
@@ -1,13 +1,20 @@
 // problem source: https://leetcode.com/problems/subarray-sum-equals-k/
+// discussion reference: https://leetcode.com/problems/subarray-sum-equals-k/discuss/1390419/O(n)-using-hashmap-in-c%2B%2B
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int nums_size = nums.size();
+        int sum = 0;
+        int count = 0;
+        unordered_map<int, int> data;
         for (int i = 0; i < nums_size; i++) {
-            cout << nums[i] << " ";
+            sum += nums[i];
+            if (sum == k)
+                count++;
+            if (data.find(sum - k) != data.end()) // found
+                count += data[sum - k];
+            data[sum]++;
         }
-        cout << endl;
-        cout << k << endl;
-        return 0;
+        return count;
     }
 };
