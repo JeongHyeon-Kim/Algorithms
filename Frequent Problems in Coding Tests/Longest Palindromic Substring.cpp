35eb5ddc8a10ecb770e75eca9d89c6b45235418b
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-12 17:28:01 +0900

    Uploaded basic code of 'Longest Palindromic Substring'

---
+++ Frequent Problems in Coding Tests/Longest Palindromic Substring.cpp
@@ -0,0 +1,11 @@
+// problem source: https://leetcode.com/problems/longest-palindromic-substring/
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int s_size = s.size();
+        string result;
+        for (int i = 0; i < s_size; i++)
+            cout << s[i] << " ";
+        return result;
+    }
+};
