625bd75c62baae44cd13229c744293c1d01786ac
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-13 18:59:35 +0900

    Uploaded basic code of 'Integer to English Words'

---
+++ Frequent Problems in Coding Tests/Integer to English Words.cpp
@@ -0,0 +1,9 @@
+// problem source: https://leetcode.com/problems/integer-to-english-words/
+class Solution {
+public:
+    string numberToWords(int num) {
+        string result;
+        cout << num << endl;
+        return result;
+    }
+};
