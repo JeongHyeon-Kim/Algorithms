fe39a839406d3d6f076bbbcf8f73344c3617fe38
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-16 17:17:14 +0900

    Solved 'Integer to English Words'

--- Frequent Problems in Coding Tests/Integer to English Words.cpp
+++ Frequent Problems in Coding Tests/Integer to English Words.cpp
@@ -1,45 +1,40 @@
 // problem source: https://leetcode.com/problems/integer-to-english-words/
+// discussion reference: https://leetcode.com/problems/integer-to-english-words/discuss/1327159/C%2B%2B-clean-recursive-code
 class Solution {
 public:
-    string numberToWords(int num) {
-        string one_unit[10] = {" ", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-        string under_twenty_unit[10] = {" ", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-        string ten_unit[10] = {" ", "Ten", "Twenty", "Thirty", "Forty", "Sixty", "Seventy", "Eighty", "Ninety"};
-        string bigger_unit[4] = {"Hundred", "Thousand", "Million", "Billion"};
-        string result;
-        int rotation = 0;
-        int bigger_index = 0;
-        while (num != 0) { // 123 -> 12 -> 1 -> 0 end
-            int number_index = num % 10; // 3 -> 2 -> 1
-            num = num / 10;
-            if (rotation % 4 == 0) {
-                result.insert(0, one_unit[number_index]);
-                rotation++;
-                if (num != 0)
-                    result.insert(0, " ");
-            } else if (rotation % 4 == 1) {
-                result.insert(0, ten_unit[number_index]);
-                rotation++;
-                if (num != 0)
-                    result.insert(0, " ");
-            } else if (rotation % 4 == 2) {
-                result.insert(0, bigger_unit[bigger_index % 4]);
-                result.insert(0, " ");
-                result.insert(0, one_unit[number_index]);
-                bigger_index++;
-                rotation++;
-                if (num != 0)
-                    result.insert(0, " ");
-            } else { // rotation % 4 == 3
-                result.insert(0, bigger_unit[bigger_index % 4]);
-                result.insert(0, " ");
-                result.insert(0, one_unit[number_index]);
-                bigger_index++;
-                rotation++;
-                if (num != 0)
-                    result.insert(0, " ");
-            }
+    string one_unit[10] = {" ", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine "};
+    string under_twenty_unit[10] = {"Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ", "Eighteen ", "Nineteen "};
+    string ten_unit[10] = {" ", " ", "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety "};
+
+    string recursive_conversion(int num) {
+        if (num >= 1000000000)
+            return recursive_conversion(num / 1000000000) + "Billion " + recursive_conversion(num % 1000000000);
+        if (num >= 1000000)
+            return recursive_conversion(num / 1000000) + "Million " + recursive_conversion(num % 1000000);
+        if (num >= 1000)
+            return recursive_conversion(num / 1000) + "Thousand " + recursive_conversion(num % 1000);
+        if (num >= 100)
+            return recursive_conversion(num / 100) + "Hundred " + recursive_conversion(num % 100);
+        if (num >= 20) {
+            int tens =  num / 10;
+            return tens[ten_unit] + recursive_conversion(num % 10);
         }
+        if (num >= 10 && num < 20)
+            return under_twenty_unit[num % 10];
+        if (num < 10)
+            return one_unit[num % 10];
+        return "";
+    }
+
+    string numberToWords(int num) {
+        if (num  == 0)
+            return "Zero";
+        string result, individual_word;
+        stringstream extract;
+        extract.str(recursive_conversion(num)); // set string
+        while (extract >> individual_word) // extract same type of string
+            result += individual_word + " ";
+        result.pop_back(); // remove last blank
         return result;
     }
 };
