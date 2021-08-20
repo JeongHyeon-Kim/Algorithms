2849486f00e9613ae05595bc232463cc9ad7f0fb
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-20 23:57:00 +0900

    Uploaded basic code of 'Consecutive Numbers Sum'

---
+++ Frequent Problems in Coding Tests/Consecutive Numbers Sum.cpp
@@ -0,0 +1,8 @@
+// problem source: https://leetcode.com/problems/consecutive-numbers-sum/
+class Solution {
+public:
+    int consecutiveNumbersSum(int n) {
+        cout << n << endl;
+        return 0;
+    }
+};
