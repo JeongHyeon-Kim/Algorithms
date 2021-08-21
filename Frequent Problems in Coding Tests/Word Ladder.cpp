45d898100a638041715a95c2f1d072af9b3c18ea
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-08-22 03:40:30 +0900

    Solved 'Word Ladder'

--- Frequent Problems in Coding Tests/Word Ladder.cpp
+++ Frequent Problems in Coding Tests/Word Ladder.cpp
@@ -1,12 +1,36 @@
 // problem source: https://leetcode.com/problems/word-ladder/
+// discussion reference: https://leetcode.com/problems/word-ladder/discuss/1383702/Easy-BFS-approach-in-C%2B%2B
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int word_list_size = wordList.size();
-        cout << beginWord << endl;
-        cout << endWord << endl;
-        for (int i = 0; i < word_list_size; i++)
-            cout << wordList[i] << " ";
+        unordered_set<string>dict(wordList.begin(), wordList.end());
+        queue<string> data;
+        int step = 1; // begin and end word's diff is one character at least
+        if (dict.find(endWord) == dict.end()) // not found
+            return 0;
+        data.push(beginWord);
+        while (!data.empty()) {
+            int data_size = data.size();
+            while (data_size) {
+                string node = data.front();
+                data.pop();
+                if (node == endWord)
+                    return step;
+                for (int i = 0; i < node.length(); i++) {
+                    for (char search = 'a'; search <= 'z'; search++) {
+                        char target = node[i];
+                        node[i] = search;
+                        if (dict.find(node) != dict.end()) { // found
+                            dict.erase(node);
+                            data.push(node);
+                        }
+                        node[i] = target;
+                    }
+                }
+                data_size--;
+            }
+            step++;
+        }
         return 0;
     }
 };
