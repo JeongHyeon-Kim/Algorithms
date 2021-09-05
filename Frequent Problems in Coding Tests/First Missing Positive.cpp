75255258c271cb48f1f0c460e2e1e5c199842228
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-09-05 17:15:38 +0900

    Uploaded middle solution of 'First Missing Positive'

--- Frequent Problems in Coding Tests/First Missing Positive.cpp
+++ Frequent Problems in Coding Tests/First Missing Positive.cpp
@@ -1,10 +1,18 @@
 // problem source: https://leetcode.com/problems/first-missing-positive/
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
+    int firstMissingPositive(vector<int>& nums) {        
+        // Runtime Error with [2147483647]
         int nums_size = nums.size();
-        for (int i = 0; i < nums_size; i++)
-            cout << nums[i] << " ";
+        bool checking[10000000] = {false};
+        for (int i = 0; i < nums_size; i++) {
+            if (nums[i] > 0)
+                checking[nums[i]] = true;
+        }
+        for (int i = 1; i < 10000000; i++) {
+            if (checking[i] == false)
+                return i;
+        }
         return 0;
     }
 };
