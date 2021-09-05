f11d98852fd7b772003361f6b1081066c9fb0349
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-09-05 17:09:35 +0900

    Uploaded basic code of 'First Missing Positive'

---
+++ Frequent Problems in Coding Tests/First Missing Positive.cpp
@@ -0,0 +1,10 @@
+// problem source: https://leetcode.com/problems/first-missing-positive/
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int nums_size = nums.size();
+        for (int i = 0; i < nums_size; i++)
+            cout << nums[i] << " ";
+        return 0;
+    }
+};
