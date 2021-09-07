7cfc90555ad3b0ed8e83f5789a4a9df4f49655dc
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-09-07 23:56:37 +0900

    Uploaded basic code of 'ZigZag Conversion'

---
+++ Frequent Problems in Coding Tests/ZigZag Conversion.cpp
@@ -0,0 +1,9 @@
+// problem source: https://leetcode.com/problems/zigzag-conversion/
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        cout << s << endl;
+        cout << numRows << endl;
+        return "";
+    }
+};
