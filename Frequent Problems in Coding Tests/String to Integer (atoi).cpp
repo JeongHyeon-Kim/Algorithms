33608dee4f867bc04b2adc7485e1feac91a93cd6
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-09-08 23:43:18 +0900

    Uploaded basic code of 'String to Integer (atoi)'

---
+++ Frequent Problems in Coding Tests/String to Integer (atoi).cpp
@@ -0,0 +1,8 @@
+// problem source: https://leetcode.com/problems/string-to-integer-atoi/
+class Solution {
+public:
+    int myAtoi(string s) {
+        cout << s << endl;
+        return 0;
+    }
+};
