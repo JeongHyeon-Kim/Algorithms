90df55c8519f534bfb4e0fa03b53f8ed4caf7e50
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-09-08 23:43:43 +0900

    Solved 'String to Integer (atoi)'

--- Frequent Problems in Coding Tests/String to Integer (atoi).cpp
+++ Frequent Problems in Coding Tests/String to Integer (atoi).cpp
@@ -1,8 +1,11 @@
 // problem source: https://leetcode.com/problems/string-to-integer-atoi/
+// discussion reference: https://leetcode.com/problems/string-to-integer-atoi/discuss/1451020/simple-C%2B%2B-using-istringstream%3A
 class Solution {
 public:
     int myAtoi(string s) {
-        cout << s << endl;
-        return 0;
+        istringstream data(s);
+        int result = 0;
+        data >> result;
+        return result;
     }
 };
