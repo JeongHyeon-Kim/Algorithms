30bd608d105810e0dc8a92c9ea12ac5f1b9c2708
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-09-10 00:21:13 +0900

    Uploaded basic code of 'Palindrome Number'

---
+++ Frequent Problems in Coding Tests/Palindrome Number.cpp
@@ -0,0 +1,8 @@
+// problem source: https://leetcode.com/problems/palindrome-number/
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        cout << x << endl;
+        return true;
+    }
+};
