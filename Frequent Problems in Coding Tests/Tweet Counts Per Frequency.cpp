688fdcc8d83a064db12b407ffec6455ae998fcbe
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-10-05 20:28:48 +0900

    Uploaded middle solution of 'Tweet Counts Per Frequency'

--- Frequent Problems in Coding Tests/Tweet Counts Per Frequency.cpp
+++ Frequent Problems in Coding Tests/Tweet Counts Per Frequency.cpp
@@ -1,16 +1,49 @@
 // problem source: https://leetcode.com/problems/tweet-counts-per-frequency/
 class TweetCounts {
 public:
-    TweetCounts() {
-
-    }
+    vector<int> result;
+    vector<pair<string, int>> recording;
+    TweetCounts() { }
 
     void recordTweet(string tweetName, int time) {
-
+        recording.push_back(make_pair(tweetName, time));
     }
 
     vector<int> getTweetCountsPerFrequency(string freq, string tweetName, int startTime, int endTime) {
+        int recording_size = recording.size();
+        int middle_solution[10001] = {0, };
+        int chunk_unit = 0;
+        result.clear();
 
+        for (int i = 0; i < recording_size; i++) {
+            if (recording[i].first == tweetName) {
+                if (freq == "minute")
+                    chunk_unit = 60;
+                else if (freq == "hour")
+                    chunk_unit = 3600;
+                else // freq == "day"
+                    chunk_unit = 864000;
+
+
+                for (int j = startTime; j <= endTime; j += chunk_unit) {
+                    if (recording[i].second >= startTime && recording[i].second <= endTime)
+                        middle_solution[j / chunk_unit]++;
+                }
+            }
+        }
+        for (int i = startTime; i <= endTime; i += chunk_unit) {
+            if (middle_solution[i / chunk_unit] != 0)
+                result.push_back(middle_solution[i]);
+            else
+                result.push_back(0);
+        }
+        // for (int i = 0; i < 10001; i++) {
+        //     if (middle_solution[i] != 0)
+        //         result.push_back(middle_solution[i]);
+        // }
+        // if (result.empty())
+        //     result.push_back(0);
+        return result;
     }
 };
 
