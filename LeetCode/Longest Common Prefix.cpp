03a44f6aae11cf376111692001db654592f60ae5
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-10-07 23:57:11 +0900

    Uploaded basic code of 'Longest Common Prefix'

---
+++ LeetCode/Longest Common Prefix.cpp
@@ -0,0 +1,11 @@
+// problem source: https://leetcode.com/problems/longest-common-prefix/
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string result;
+        int strs_size = strs.size();
+        for (int i = 0; i < strs_size; i++)
+            cout << strs[i] << endl;
+        return result;
+    }
+};
