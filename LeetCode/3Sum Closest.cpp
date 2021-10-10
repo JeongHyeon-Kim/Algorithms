3d6bf961921c4ab1c8ddcfdb2dd190355566ea8a
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-10-10 23:48:51 +0900

    Uploaded basic code of '3Sum Closest'

---
+++ LeetCode/3Sum Closest.cpp
@@ -0,0 +1,12 @@
+// problem source: https://leetcode.com/problems/3sum-closest/
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int nums_size = nums.size();
+        for (int i = 0; i < nums_size; i++)
+            cout << nums[i] << " ";
+        cout << endl;
+        cout << target << endl;
+        return 0;
+    }
+};
