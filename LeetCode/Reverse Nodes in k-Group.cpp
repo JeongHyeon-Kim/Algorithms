8b6049ac579f69b12110a1e4513fffe68438c6d2
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-10-30 01:01:45 +0900

    Solved 'Reverse Nodes in k-Group'

--- LeetCode/Reverse Nodes in k-Group.cpp
+++ LeetCode/Reverse Nodes in k-Group.cpp
@@ -13,14 +13,35 @@ class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         auto it = head;
+        vector<int> middle_solution;
+
         if (k == 1)
             return head;
+
         while (it != nullptr) {
-            cout << it->val << " ";
+            middle_solution.push_back(it->val);
             it = it->next;
         }
-        cout << endl;
-        cout << k << endl;
-        return head;
+
+        int node_count = middle_solution.size();
+        int iteration = node_count / k;
+
+        for (int i = 0; i < iteration; i++) {
+            for (int j = 0; j < (k / 2); j++) {
+                int temp = middle_solution[i * k + j]; // i * k is jumping next group
+                middle_solution[i * k + j] = middle_solution[(k - 1) + (i * k) - j]; // (k - 1) is in group element from backward
+                middle_solution[(k - 1) + (i * k) - j] = temp; // j is backward index
+            }
+        }
+
+        ListNode* result = new ListNode(middle_solution[0]);
+        auto it2 = result;
+        for (int i = 1; i < node_count; i++) {
+            ListNode* newNode = new ListNode(middle_solution[i]);
+            it2->next = newNode;
+            it2 = it2->next;
+        }
+
+        return result;
     }
 };
