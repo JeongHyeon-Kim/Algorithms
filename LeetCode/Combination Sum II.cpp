49a5ef70a21f58623406011f7084b461c7d0fafa
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2021-11-27 15:40:12 +0900

    Solved 'Combination Sum II'

--- LeetCode/Combination Sum II.cpp
+++ LeetCode/Combination Sum II.cpp
@@ -1,13 +1,31 @@
 // problem source: https://leetcode.com/problems/combination-sum-ii/
+// discussion reference: https://leetcode.com/problems/combination-sum-ii/discuss/16861/Java-solution-using-dfs-easy-understand
 class Solution {
 public:
+    // java to c++
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        int candidates_size = candidates.size();
-        vector<vector<int>> result;
-        for (int i = 0; i < candidates_size; i++)
-            cout << candidates[i] << " ";
-        cout << endl;
-        cout << target << endl;
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> result; // List<List<Integer>> result = new ArrayList<List<Integer>>();
+        vector<int> path; // List<Integer> path = new ArrayList<Integer>();
+        dfs_solution(candidates, 0, target, path, result);
         return result;
+    }
+
+    void dfs_solution(vector<int> candidates, int current_index, int target, vector<int> path, vector<vector<int>>& result) { // int[] canditates, int current_index, List<Integer> path, List<List<Integer>> result
+        if (target == 0) {
+            result.push_back(path); // found, making result // result.add(new ArrayList(path));
+            return;
+        }
+
+        if (target < 0) return;
+
+        for (int i = current_index; i < candidates.size(); i++) {
+            if (i > current_index && candidates[i] == candidates[i - 1])
+                continue;
+            path.push_back(candidates[i]); // path.add(path.size(), cand[i]);
+            dfs_solution(candidates, i + 1, target - candidates[i], path, result);
+            path.pop_back(); // path.remove(path.size() - 1);
+        }
+
     }
 };
