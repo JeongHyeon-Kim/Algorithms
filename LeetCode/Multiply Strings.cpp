3d01b07033ffe595db31e579d6c64bf28d051056
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2022-01-25 23:59:43 +0900

    Uploaded middle solution of 'Multiply Strings'

--- LeetCode/Multiply Strings.cpp
+++ LeetCode/Multiply Strings.cpp
@@ -2,8 +2,10 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
+        // Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
         string result;
-        cout << num1 << " " << num2 << endl;
+        int middle_result = stoi(num1) * stoi(num2);
+        result = to_string(middle_result);
         return result;
     }
 };
