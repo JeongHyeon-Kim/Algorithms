706516237f96ce7d69d86e5066848dddca8456b9
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2022-01-25 23:54:24 +0900

    Uploaded basic code of 'Multiply Strings'

---
+++ LeetCode/Multiply Strings.cpp
@@ -0,0 +1,9 @@
+// problem source: https://leetcode.com/problems/multiply-strings/
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        string result;
+        cout << num1 << " " << num2 << endl;
+        return result;
+    }
+};
