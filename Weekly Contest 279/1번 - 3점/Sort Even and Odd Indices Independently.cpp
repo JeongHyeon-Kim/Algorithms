a0e6f5c903682548437f20b0e486485a42c49d0e
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2022-02-06 11:50:32 +0900

    Uploaded basic code of 'Sort Even and Odd Indices Independently'

---
+++ Weekly Contest 279/1번 - 3점/Sort Even and Odd Indices Independently.cpp
@@ -0,0 +1,11 @@
+// problem source: https://leetcode.com/contest/weekly-contest-279/problems/sort-even-and-odd-indices-independently/
+class Solution {
+public:
+    vector<int> sortEvenOdd(vector<int>& nums) {
+        int nums_size = nums.size();
+        for (int i = 0; i < nums_size; i++) {
+            cout << nums[i] << endl;
+        }
+        return nums;
+    }
+};
