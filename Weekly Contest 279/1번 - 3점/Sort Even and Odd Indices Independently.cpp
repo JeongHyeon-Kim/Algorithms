91775ea3e38bff7fd0c5a6508f78c1c93f27443b
JeongHyeon-Kim <jeonghyeon.kim0709@gmail.com>
2022-02-06 12:11:17 +0900

    Solved 'Sort Even and Odd Indices Independently'

--- Weekly Contest 279/1번 - 3점/Sort Even and Odd Indices Independently.cpp
+++ Weekly Contest 279/1번 - 3점/Sort Even and Odd Indices Independently.cpp
@@ -3,8 +3,22 @@ class Solution {
 public:
     vector<int> sortEvenOdd(vector<int>& nums) {
         int nums_size = nums.size();
-        for (int i = 0; i < nums_size; i++) {
-            cout << nums[i] << endl;
+        for (int i = 0; i < nums_size - 2; i++) {
+            for (int j = i + 2; j < nums_size; j += 2) {
+                if (i % 2 == 1) {
+                    if (nums[i] < nums[j]) {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                } else {
+                    if (nums[i] > nums[j]) {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
         }
         return nums;
     }
