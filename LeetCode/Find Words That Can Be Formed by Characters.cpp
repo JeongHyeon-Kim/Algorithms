449221f2493e39d748ebacc365617aef945ec29c
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2022-07-12 21:41:45 +0900

    Uploaded basic code code of 'Find Words That Can Be Formed by Characters'

---
+++ LeetCode/Find Words That Can Be Formed by Characters.cpp
@@ -0,0 +1,11 @@
+// problem source: https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        int words_size = words.size();
+        for (int i = 0; i < words_size; i++)
+            cout << words[i] << endl;
+        cout << chars << endl;
+        return 0;
+    }
+};
