fa4ff35e763bf53d8461956a19d1fff2c1086ec6
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2023-01-28 15:28:50 +0900

    Uploaded basic code of 'Wildcard Matching'

---
+++ LeetCode/Wildcard Matching.cpp
@@ -0,0 +1,8 @@
+// problem source: https://leetcode.com/problems/wildcard-matching/
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        cout << s << " " << p << endl;
+        return false;
+    }
+};
