f36e05392bb8ebc38ee5d1a5cfe79bdc5fe06051
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2023-05-18 20:56:20 +0900

    Uploaded middle solution of 'Spiral Matrix II'

--- LeetCode/Spiral Matrix II.cpp
+++ LeetCode/Spiral Matrix II.cpp
@@ -3,7 +3,12 @@ public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>> result;
         cout << n << endl;
-        
+        int total_count = n * n;
+        if (total_count == 1) {
+            vector<int> middle_result;
+            middle_result.push_back(total_count);
+            result.push_back(middle_result);
+        }
         return result;
     }
 };
