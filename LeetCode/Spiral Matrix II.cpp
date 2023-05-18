efe1e22ee41585af7517279fda1f6da47bdcc9d5
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2023-05-18 21:17:21 +0900

    Solved 'Spiral Matrix II'

--- LeetCode/Spiral Matrix II.cpp
+++ LeetCode/Spiral Matrix II.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> result;
-        cout << n << endl;
-        int total_count = n * n;
-        if (total_count == 1) {
-            vector<int> middle_result;
-            middle_result.push_back(total_count);
-            result.push_back(middle_result);
+        vector<vector<int>> result(n, vector<int>(n));
+        int numbering = 1;
+        int row = 0;
+        int column = 0;
+        int direction = 0;
+        vector<int> direction_row = {0, 1, 0, -1};
+        vector<int> direction_column = {1, 0, -1, 0};
+
+        while (numbering <= n * n) {
+            result[row][column] = numbering; // insert number
+            numbering++; // increase number
+            // change direction
+            int next_row = row + direction_row[direction];
+            int next_column = column + direction_column[direction];
+            if (next_row < 0 || next_row >= n || next_column < 0 || next_column >= n 
+            || result[next_row][next_column] != 0) {
+                direction = (direction + 1) % 4; // four direction
+            }
+            row += direction_row[direction];
+            column += direction_column[direction];
         }
         return result;
     }
