1984f91053eac7cca7c227024dbf4ccb2035f2a1
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2023-05-18 20:55:51 +0900

    Uploaded basic code of 'Spiral Matrix II'

---
+++ LeetCode/Spiral Matrix II.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> result;
+        cout << n << endl;
+        
+        return result;
+    }
+};
