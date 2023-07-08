337f553c1acccf86124ac43a79e03bd47349bef9
김정현 (JeongHyeon Kim) <jeonghyeon.kim0709@gmail.com>
2023-07-08 17:01:16 +0900

    Solved 'Permutation Sequence'

--- LeetCode/Permutation Sequence.cpp
+++ LeetCode/Permutation Sequence.cpp
@@ -1,4 +1,28 @@
 // problem source: https://leetcode.com/problems/permutation-sequence/
+// discussion reference: https://leetcode.com/problems/permutation-sequence/solutions/2502436/c-100-fastest-solution-best-approach-with-good-explanation-easy-to-understand-1/
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        int factorial = 1;
+        vector<int> numbers;
+        for (int i = 1; i < n; i++) {
+            factorial = factorial * i;
+            numbers.push_back(i);
+        }
+        numbers.push_back(n);
+        k = k - 1;
+        string answer = "";
+        while (true) {
+            answer = answer + to_string(numbers[k / factorial]);
+            numbers.erase(numbers.begin() + k / factorial);
+            if (numbers.size() == 0)
+                break;
+            k = k % factorial;
+            factorial = factorial / numbers.size();
+        }
+        return answer;
+    }
+};
 
 /*
 순열에 대한 조합의 경우들을 vector에 저장하고 k-1 번째의 요소를 출력한다.
